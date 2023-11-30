d7b0c9a29a6675e5fa2696b940189ad0a86e330e
Joshua Garcia <josshuagarciia000@gmail.com>
2023-11-29 16:05:29 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+const papel = document.querySelector("#papel");
+const piedra = document.querySelector("#piedra");
+const tijeras =document.querySelector("#tijeras");
+
+papel.addEventListener('click', () => {
+    console.log("usuario le dio click a nuestra foto")
+})
+
+piedra.addEventListener('click', () => {
+    console.log("usuario escojio piedra")
+})
+
+tijeras.addEventListener('click', () => {
+    console.log("usuario escojio tijeras")
+})
+
+
+// arrays -> una estructura de datos que guarda informacion del mismo tipo
+
+// estructura de datos: una manera de organizar informacion 
+// array: una estructura simple donde la infomacion tiene un lugar especifico 
+
+// array: tiene mucho espacios y cada espacio contiene informacion 
+// array: cada espacio tiene un numero asociado, llamado indice
+
+
+
+
+
+
+
